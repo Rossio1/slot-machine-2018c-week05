294054190f6a6813119dcd3f9700f237ad27a926
Rossi01 <rosegoldith@gmail.com>
2018-10-19 23:53:23 -0400

    answer branch

---
+++ main.js
@@ -0,0 +1,130 @@
+// // Set up event listeners for each button by their id
+
+// // Start to run the real with the start button being clicked
+//
+// // Use the interval method to iterate through your array of images
+//
+// // Use the clear interval method to stop your interval method and set the image src to the img tag
+//
+// // Have a win condition to check if the source of the images are all the same
+
+var interval;
+var interval2;
+var interval3;
+var stopRotate1 = false
+var stopRotate2 = false
+var stopRotate3 = false
+var reel1 = null
+var reel2 = null
+var reel3 = null
+var pot = 0
+var bank = 100
+
+
+var imgArr = ['images/cherry.png', 'images/club.png', 'images/orange.png', 'images/seven.png', 'images/bar.png']
+
+window.addEventListener('load', function(){
+  document.getElementById('pot').innerHTML = 'Pot: ' + pot
+  document.getElementById('bank').innerHTML = 'Bank: ' + bank
+})
+
+document.getElementById('start').onclick = startGame
+document.getElementById('max').onclick = maxBet
+document.getElementById('min').onclick = minBet
+document.getElementById('stop1').onclick = stop1
+document.getElementById('stop2').onclick = stop2
+document.getElementById('stop3').onclick = stop3
+
+function maxBet(){
+  pot += 5 * 2
+  bank -= 5
+  document.getElementById('pot').innerHTML = 'Pot: ' + pot
+  document.getElementById('bank').innerHTML = 'Bank: ' + bank
+}
+
+function minBet(){
+  pot += 2 * 2
+  bank -= 2
+  document.getElementById('pot').innerHTML = 'Pot: ' + pot
+  document.getElementById('bank').innerHTML = 'Bank: ' + bank
+}
+
+function startGame(){
+  if(pot === 0){
+    document.getElementById('msg').innerHTML = 'Place a bet!'
+  }else{
+    spin1()
+    spin2()
+    spin3()
+  }
+}
+
+function spin1(){
+  // Inside the setInterval parameter (rotate1 is the function, 10000 is the amount of time by milisceonds)
+  interval1 = setInterval(rotate1, 1000)
+}
+
+function spin2(){
+  interval2 = setInterval(rotate2, 1000)
+}
+
+function spin3(){
+  interval3 = setInterval(rotate3, 1000)
+}
+
+function rotate1(){
+  var img = document.getElementsByTagName('img')[0]
+  var image = imgArr[Math.floor(Math.random() * imgArr.length)]
+  img.src = image
+  reel1 = image
+}
+
+function rotate2(){
+  var img = document.getElementsByTagName('img')[1]
+  var image = imgArr[Math.floor(Math.random() * imgArr.length)]
+  img.src = image
+  reel2 = image
+}
+
+function rotate3(){
+  var img = document.getElementsByTagName('img')[2]
+  var image = imgArr[Math.floor(Math.random() * imgArr.length)]
+  img.src = image
+  reel3 = image
+}
+
+function stop1(){
+  clearInterval(interval1)
+  stopRotate1 = true
+  if(stopRotate1 == true && stopRotate2 == true && stopRotate3 == true){
+    winCondition()
+  }
+}
+function stop2(){
+  clearInterval(interval2)
+  stopRotate2 = true
+  if(stopRotate1 == true && stopRotate2 == true && stopRotate3 == true){
+    winCondition()
+  }
+}
+function stop3(){
+  clearInterval(interval3)
+  stopRotate3 = true
+  if(stopRotate1 == true && stopRotate2 == true && stopRotate3 == true){
+    winCondition()
+  }
+}
+
+function winCondition(){
+  if(reel1 == reel2 && reel1 == reel3){
+    bank += pot
+    pot = 0
+    document.getElementById('pot').innerHTML = 'Pot: ' + pot
+    document.getElementById('bank').innerHTML = 'Bank: ' + bank
+    document.getElementById('msg').innerHTML = 'Winner Winner Chicken Dinner'
+  }else{
+    pot = 0
+    document.getElementById('pot').innerHTML = 'Pot: ' + pot
+      document.getElementById('msg').innerHTML = 'Game Over'
+  }
+}
